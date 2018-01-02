4171a9339ce60addfaf53d015e95098771bfaf78
nazila86 <nzl65@yahoo.com>
2018-01-02 18:09:20 +0330

    open wifi setting if it was disconnected

--- app/src/main/java/com/android/sunshine/HomeActivity.java
+++ app/src/main/java/com/android/sunshine/HomeActivity.java
@@ -2,16 +2,15 @@ package com.android.sunshine;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.wifi.WifiManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.sunshine.helpers.NetworkStatus;
 
@@ -21,7 +20,6 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     private TextView mNoNetworkTextView;
     private RelativeLayout mHomeActivityLayout;
     private Button mWifiSettingsButton;
-    private boolean checkNetworkNow=true;
 
     public static void startHomeActivity(Context context) {
         Intent homeActivityIntent = new Intent(context, HomeActivity.class);
@@ -35,67 +33,50 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         findViews();
         mWifiSettingsButton.setOnClickListener(this);
         checkNetworkStatus();
-
-      //  if (checkNetworkStatus()) {
-//            Intent i=new Intent(HomeActivity.this,ForecastListviewActivity.class);
-//            startActivity(i);
-//            CurrentWeatherActivity.startCuurentWeatherActivity(HomeActivity.this);
-//            finish();
-//        } else {
-//            mHomeActivityLayout.setVisibility(View.VISIBLE);
-//        }
     }
 
     private void findViews() {
         mHomeActivityLayout = findViewById(R.id.activityHome_layout);
         mNoNetworkTextView = findViewById(R.id.activityHome_textView_noNetwork);
-        mWifiSettingsButton=findViewById(R.id.activityHome_button_wifiSettings);
+        mWifiSettingsButton = findViewById(R.id.activityHome_button_wifiSettings);
     }
 
     private void checkNetworkStatus() {
-        Toast.makeText(this, "checking network", Toast.LENGTH_SHORT).show();
-      //  if(checkNetworkNow) {
-            if (helperFunctions.isNetworkConnected()) {
-                CurrentWeatherActivity.startCuurentWeatherActivity(HomeActivity.this);
-                finish();
-        //    }
-            } else {
-                mHomeActivityLayout.setVisibility(View.VISIBLE);
-
+        Thread newThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    //check if connected!
+                    while (!isConnected(HomeActivity.this)) {
+                        //Wait to connect
+                        Thread.sleep(1000);
+                    }
+                    CurrentWeatherActivity.startCuurentWeatherActivity(HomeActivity.this);
+                    finish();
+                } catch (Exception e) {
+                }
             }
-
-          //  return true;      // CurrentWeatherActivity.startCuurentWeatherActivity(HomeActivity.this);
-//        } else {
-//            Toast.makeText(this, "No Internet Connection", Toast.LENGTH_LONG).show();
-//            return false;
-//        }
+        };
+        newThread.start();
     }
-
     @Override
     public void onClick(View clickedView) {
-        if(clickedView.getId()==R.id.activityHome_button_wifiSettings)
-            openNetworkSettings();
-      //  checkNetworkStatus();
-    }
-
+        if (clickedView.getId() == R.id.activityHome_button_wifiSettings)
+            openNetworkSettings();}
 
     private void openNetworkSettings() {
         Intent intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
-        startActivityForResult(intent,0);
-
+        startActivity(intent);
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent)
-    {
-        if(requestCode==0)
-        {
-            WifiManager wifiManager = (WifiManager)
-                    getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-            if(!wifiManager.isWifiEnabled())
-                Log.i("Error res:"," on result");
-           onRestart();
+    public static boolean isConnected(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (connectivityManager != null) {
+            networkInfo = connectivityManager.getActiveNetworkInfo();
         }
-    }
 
+        return networkInfo != null && networkInfo.getState() == NetworkInfo.State.CONNECTED;
+    }
 
 }
